b5e1c04ae072b46bd04d4c29c9481f9d79b95efa
FOLUWA <moronfoluwaakintola@gmail.com>
2018-06-30 13:20:02 +0100

    SW

--- currencyconverter/public/js/sw.js
+++ currencyconverter/public/js/sw.js
@@ -1,5 +1,5 @@
 //This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
-
+console.log('SW.JS LOADED SUCCESSFULLY');
 //Install stage sets up the offline page in the cache and opens a new cache
 self.addEventListener('install', function(event) {
   event.waitUntil(preLoad());
