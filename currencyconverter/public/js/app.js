5a17f2b22651a1ae4fdc5290d7d7771bf2e0e38e
FOLUWA <moronfoluwaakintola@gmail.com>
2018-06-30 13:18:09 +0100

    vm

--- currencyconverter/public/js/app.js
+++ currencyconverter/public/js/app.js
@@ -76,7 +76,7 @@ form_element.addEventListener('submit', event => {
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // IndexedDb initialization
-//const idb = "idb.js";
+const idb = 'https://foluwa.github.io/currencyconverter/public/js/sw.js';
 
 const dbPromise = idb.open('currencyConverter', 3, (upgradeDb) => {
     switch (upgradeDb.oldVersion) {
