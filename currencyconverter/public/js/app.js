1a3e40b70fb5ff26551e82a7e4a6b6a9f3fa9844
FOLUWA <moronfoluwaakintola@gmail.com>
2018-06-30 11:28:36 +0100

    minutes

--- currencyconverter/public/js/app.js
+++ currencyconverter/public/js/app.js
@@ -1,37 +1,24 @@
+console.log("FOLUWA IS HERE");
 // TO GET ALL THE CURRENCIES AVAILABLR
 fetch('https://free.currencyconverterapi.com/api/v5/currencies')
     .then(response=> {
        return response.json();
     }).then(data =>{
-    //   console.log(data);
-    //    const currencies = data.results;
-    //   for( const currency in currencies){
-    //      console.log(currency);
-          
-    //   }  
- 
-    // console.log(Object.entries(data.results));
+
     const currencyArray = Object.entries(data.results);
     let mainMap = new Map();
     for(const currency of currencyArray){
-     //   console.log(currency);
         let currencyName = currency[1].currencyName;
-       // let currencySymbole = currency[1].currencySymboll
         let currencyId = currency[1].id;
-
         mainMap.set(currency[1].id, currency[1].currencyName);
-        
-       // currencies.add(currencyId, currencyName);
-        // console.log(currencyId);
-        // console.log(currencyName);
-        // console.log(mainMap.size);   
     }
     return mainMap;
     })
+
     .then(currencyMap =>{
         firstCurrency = document.getElementById('current_currency');
         secondCurrency = document.getElementById('equivalent_currency');
-        // console.log(currencyMap.size);
+
         for (const curr of currencyMap) {
             let[id, name] = curr;
            firstCurrency.add(new Option(name, id));
@@ -57,10 +44,9 @@ form_element.addEventListener('submit', event => {
         // console.log(data);
         const queryResult = Object.entries(data.results);
         //  console.log(queryResult);
-        // console.log(queryResult[0][1].val);
         let rate = queryResult[0][1].val;
         return rate;
-        //////////////
+        ///////////////////////////////////////
     }).then(rate => {
         let amount_Field = document.getElementById('current_amount').value;
         let converted_Value = rate * amount_Field;
@@ -69,6 +55,16 @@ form_element.addEventListener('submit', event => {
 });
 
 //SERVICE WORKER
+  if ('serviceWorker' in navigator) {
+       navigator.serviceWorker.register('https://foluwa.github.io/currencyconverter/public/js/sw.js').then((registration) => {
+        console.log('Registration successful, scope is:', registration.scope);
+       })
+       .catch((error) => {
+          console.log('Service worker registration failed, error:', error);
+       });
+
+  }
+/*
 if ('serviceWorker' in navigator) { //currencyconverter/public/js/sw.js   /try to use th file system fir this dont not go to https
     navigator.serviceWorker.register('https://foluwa.github.io/currencyconverter/public/js/sw.js').then(function(registration) {
       console.log('Registration successful, scope is:', registration.scope);
@@ -76,4 +72,9 @@ if ('serviceWorker' in navigator) { //currencyconverter/public/js/sw.js   /try t
     .catch(function(error) {
       console.log('Service worker registration failed, error:', error);
     });
-  }
+  }*/
+
+
+
+/////////////////////////////////////////////////////////////////////////////////
+
