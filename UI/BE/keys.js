901e54e204ec06245ef04995ec4f9ea7bac50368
MORONFOLUWA AKINTOLA <moronfoluwaakintola@gmail.com>
2019-09-13 21:53:12 +0100

    I added routesz

--- UI/BE/keys.js
+++ UI/BE/keys.js
@@ -1,24 +1,5 @@
-
 module.exports = {
-    mongodb:{
-        dbURI: 'mongodb+srv://rcfosogbo:admin2019@rcfosogbo@rcfcluster-lnerm.gcp.mongodb.net/test?retryWrites=true'
+    mongodb: {
+        dbURI: ""
     }
 };
-
-//mongodb+srv://rcfosogbo:admin2019@rcfosogbo@rcfcluster-lnerm.gcp.mongodb.net/test?retryWrites=true
-//mongodb://localhost/testForAuth
-
-
-
-//<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-//<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-
-//<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-countto/1.2.0/jquery.countTo.min.js"></script>
-//<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.compatibility.min.js"></script>
-//<script src="https://cdnjs.cloudflare.com/ajax/libs/flexslider/2.6.0/jquery.flexslider-min.js"></script>
-//<script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/0.9.9/jquery.magnific-popup.min.js"></script>
-//<script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.0/jquery.waypoints.min.js"></script>
-//<script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
-//<script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
-//<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.0/owl.carousel.min.js"></script>
-//<script src="https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
